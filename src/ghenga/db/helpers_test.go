82a6e8587133134f2b513c47502a25f37b76fa4b
Alexander Neumann <alexander@bumpern.de>
2016-07-01 11:38:41 +0200

    Add test for NewFakeUser()

--- src/ghenga/db/helpers_test.go
+++ src/ghenga/db/helpers_test.go
@@ -12,3 +12,14 @@ func TestNewFakePerson(t *testing.T) {
 		t.Fatalf("NewFakePerson() not valid: %v", err)
 	}
 }
+
+func TestNewFakeUser(t *testing.T) {
+	u, err := NewFakeUser("de")
+	if err != nil {
+		t.Fatalf("new user creation failed: %v", err)
+	}
+
+	if !u.CheckPassword("geheim") {
+		t.Fatalf("fake user does not have password 'geheim'")
+	}
+}
